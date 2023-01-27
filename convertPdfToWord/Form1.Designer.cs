33136f4e170b198404a1ae5520bb81931248c227
eng-muhammedabdulrahman <eng-muhammed.abdulrahman@outlook.com>
2023-01-28 00:35:26 +0200

    first commit

---
+++ convertPdfToWord/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace convertPdfToWord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.bx1 = new System.Windows.Forms.TextBox();
+            this.b1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select PDF File:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // bx1
+            // 
+            this.bx1.Location = new System.Drawing.Point(57, 59);
+            this.bx1.Multiline = true;
+            this.bx1.Name = "bx1";
+            this.bx1.Size = new System.Drawing.Size(605, 32);
+            this.bx1.TabIndex = 1;
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.Gold;
+            this.b1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(668, 59);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(123, 32);
+            this.b1.TabIndex = 2;
+            this.b1.Text = "Browse File";
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.openfile);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(293, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Convert to Word";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(807, 476);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.bx1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "pdf to word converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox bx1;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ convertPdfToWord/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// support pdf to word process
+using Spire.Pdf;
+// support C# File, IO
+using System.IO;
+// open executable file at runtime
+using System.Diagnostics;
+namespace convertPdfToWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        // define the global string variable
+        string path = "";
+        // Browse Button
+        private void openfile(object sender, EventArgs e)
+        {
+        // create open file dialog object
+            OpenFileDialog fd = new OpenFileDialog();
+        // set filter option
+            fd.Filter = "PDF Files (*.pdf)|*.pdf|All Files (*.*)|(*.*)";
+        // show the dialog using its instance method and verifiy it using
+        // its built-in constant properties
+        // it returns the full path of the file, if user selects any pdf file else returns null
+            if(fd.ShowDialog()==DialogResult.OK)
+            {
+        //get the selected file path and save it to global variable path
+                path= fd.FileName;
+        // assign this selected file path to text box bx1
+                bx1.Text = fd.FileName;
+        // thats all for this button code 
+            }
+        }
+        // Convert Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+        // Create an object for PDF Document cless
+                PdfDocument obj = new PdfDocument();
+        // load the selected pdf file (via open file dialog) to the instance method of the above class
+                obj.LoadFromFile(path);
+                // start the convert process and save the resultant word document using
+                // its instance method called SaveToFile()
+                obj.SaveToFile("ConvertW.docx", FileFormat.DOCX);
+                // here i ve given the current project path of resultant word file
+                // you can give either current path or aboslute path
+                // now check whether the new docx will be created or not using Exists() method of file class
+                // if the condition below returns true means, the output word document will be ready
+                if (File.Exists("ConvertW.docx")==true)
+                {
+                    // now we need to open the created word document istantly
+                    // this is done by using the static method of Process class called Start (path)
+                    Process.Start("ConvertW.docx");
+                    // now build and run the project....
+                }
+            }
+            catch (Exception ext)
+            {
+                System.Console.WriteLine(ext.Message);
+            }
+        }
+    }
+}
+// Now I will show my project folder in my PC
+// because resultant word file be saved to current project folder....
+// Now I will run...
